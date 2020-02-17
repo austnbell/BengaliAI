39e6002b3e321a7ab3d57b83785d179ba9cd677e
Ziyin Wang <1025316069@qq.com>
2020-02-16 20:44:20 -0500

    Update wrapperModel.py

--- model/wrapperModel.py
+++ model/wrapperModel.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Feb 12 12:01:40 2020
-
 Wrapper model class
-
 takes whatever model file and runs it 
-
 @author: Austin Bell
 """
 import torch
@@ -20,13 +17,14 @@ from utils.evalUtils import accuracy, macro_recall
 # just requires setting our predictor to our core model class 
 # predictor must return predictions for each of our labels 
 class BengaliClassifier(nn.Module):
-    def __init__(self, predictor, n_grapheme=168, n_vowel=11, n_consonant=7):
+    def __init__(self, predictor, n_grapheme=168, n_vowel=11, n_consonant=7,data_type='train'):
         super(BengaliClassifier, self).__init__()
         self.n_grapheme = n_grapheme
         self.n_vowel = n_vowel
         self.n_consonant = n_consonant
         self.n_total_class = self.n_grapheme + self.n_vowel + self.n_consonant
         self.predictor = predictor
+        self.data_type = data_type
 
         self.metrics_keys = [
             'loss', 'loss_grapheme', 'loss_vowel', 'loss_consonant',
@@ -34,33 +32,37 @@ class BengaliClassifier(nn.Module):
 
     def forward(self, x, y=None):
         pred = self.predictor(x)
+        data_type = self.data_type
         if isinstance(pred, tuple):
             assert len(pred) == 3
             preds = pred
         else:
             assert pred.shape[1] == self.n_total_class
             preds = torch.split(pred, [self.n_grapheme, self.n_vowel, self.n_consonant], dim=1)
-            
+           
         # compute our individual losses and generate single loss value
         # TODO: test other loss functions
-        loss_grapheme = F.cross_entropy(preds[0], y[:, 0])
-        loss_vowel = F.cross_entropy(preds[1], y[:, 1])
-        loss_consonant = F.cross_entropy(preds[2], y[:, 2])
-        loss = loss_grapheme + loss_vowel + loss_consonant
+        if data_type == 'train':
+            loss_grapheme = F.cross_entropy(preds[0], y[:, 0])
+            loss_vowel = F.cross_entropy(preds[1], y[:, 1])
+            loss_consonant = F.cross_entropy(preds[2], y[:, 2])
+            loss = loss_grapheme + loss_vowel + loss_consonant
         
         # metric summary
-        metrics = {
-            'loss': loss.item(),
-            'loss_grapheme': loss_grapheme.item(),
-            'loss_vowel': loss_vowel.item(),
-            'loss_consonant': loss_consonant.item(),
-            'acc_grapheme': accuracy(preds[0], y[:, 0]),
-            'acc_vowel': accuracy(preds[1], y[:, 1]),
-            'acc_consonant': accuracy(preds[2], y[:, 2])
-            #'weighted_recall': macro_recall(preds, y) # will figure this out later
-        }
+            metrics = {
+                'loss': loss.item(),
+                'loss_grapheme': loss_grapheme.item(),
+                'loss_vowel': loss_vowel.item(),
+                'loss_consonant': loss_consonant.item(),
+                'acc_grapheme': accuracy(preds[0], y[:, 0]),
+                'acc_vowel': accuracy(preds[1], y[:, 1]),
+                'acc_consonant': accuracy(preds[2], y[:, 2])
+                #'weighted_recall': macro_recall(preds, y) # will figure this out later
+            }
         
-        return loss, metrics, pred
+            return loss, metrics, pred
+        else:
+            return preds
 
     # run our prediction
     def calc(self, data_loader):
@@ -87,4 +89,3 @@ class BengaliClassifier(nn.Module):
         preds = self.calc(data_loader)
         pred_labels = [torch.argmax(p, dim=1) for p in preds]
         return pred_labels
-    
