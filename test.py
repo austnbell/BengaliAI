906e480a56ab73cc8850eaaf6ddfa33ac8759f28
Ziyin Wang <1025316069@qq.com>
2020-02-16 20:43:30 -0500

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from torch.utils.data.sampler import WeightedRandomSampler
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
 import pandas as pd
+import numpy as np
 from tqdm import tqdm
 
 from model.modelBase import *
@@ -17,7 +18,7 @@ from ProcessAndAugment import *
 datadir = "./data"
 inputdir= datadir + "/raw"
 outputdir= datadir + "/processed"
-
+data_type = 'test'
 # Parameters
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 batch_size=128
@@ -44,10 +45,10 @@ def load_my_state_dict(self, state_dict):
             own_state[name].copy_(param)
 
 predictor = densenet(in_channels=1, out_dim=n_total).to(device)
-state_dict =torch.load('./savedModels/densenet_saved_weights.pth')
+state_dict =torch.load('./densenet_saved_weights.pth')
 #predictor=load_my_state_dict(model, state_dict)
 
-classifier = BengaliClassifier(predictor)
+classifier = BengaliClassifier(predictor,data_type='test')
 classifier.load_state_dict(state_dict)
 classifier = classifier.to(device)
 
@@ -56,7 +57,6 @@ classifier = classifier.to(device)
 test = pd.read_csv(datadir+'/test.csv')
 indices = [0,1,2,3] # just set to list of all indices when actually training
 submission = True
-data_type = 'test'
 dataset = genDataset(indices, inputdir, data_type = "test") # generates the dataset class
 
 # push to data loader
@@ -70,11 +70,20 @@ with torch.no_grad():
         inputs = inputs.to(device)
             
         outputs1,outputs2,outputs3 = classifier(inputs.unsqueeze(1).float())
+        print(outputs1)
         
         predictions.append(outputs3.argmax(1).cpu().detach().numpy())
-        predictions.append(outputs2.argmax(1).cpu().detach().numpy())
         predictions.append(outputs1.argmax(1).cpu().detach().numpy())
+        predictions.append(outputs2.argmax(1).cpu().detach().numpy())
 
 
 
 print(predictions)
+
+
+###submission
+
+submission = pd.read_csv('./sample_submission.csv')
+submission.target = np.hstack(np.asarray(predictions).T)
+submission.to_csv('sub_bengali.csv', index=False,)
+print(submission)
